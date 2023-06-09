13d6691f052a46cec3b1a492bee9013c79e3d5d1
etudiant <etudiant@debian>
2023-06-09 10:57:18 +0200

    jour de la finale

--- accounts/models.py
+++ accounts/models.py
@@ -15,39 +15,3 @@ class Compte(models.Model):
     connexion_date = models.DateTimeField(blank=True, null=True)
     def __str_(self):
         return self.user.username
-
-
-
-class Commu(models.Model): #Class de base
-    name= models.CharField(max_length= 19)
-    slug = models.SlugField(max_length=128)
-    marque = models.CharField(max_length= 128)
-    administrateur = models.CharField(max_length= 20)
-    zone = models.CharField(max_length= 50)
-    configuration = models.TextField(blank=True)
-    detail = models.TextField(blank=True) 
-    thumbnail = models.ImageField(upload_to="images", blank=True, null=True)
-
-    def __str__ (self):
-        return str(self.name)
-
-    def get_absolute_url(self):
-        return reverse("commu_detail", kwargs={"slug": self.slug})
-   
-
-
-class Routeur(models.Model):
-    name = models.CharField(max_length= 19)
-    slug = models.SlugField(max_length=128)
-    marque = models.CharField(max_length= 128)
-    administrateur = models.CharField(max_length= 20)
-    zone = models.CharField(max_length= 50)
-    configuration = models.TextField(blank=True)
-    detail = models.TextField(blank=True) #Pas forcement de description
-    thumbnail = models.ImageField(upload_to="images", blank=True, null=True)
-
-    def __str__ (self):
-        return str(self.name)
-        
-    def get_absolute_url(self):
-        return reverse("routeur_detail", kwargs={"slug": self.slug})
